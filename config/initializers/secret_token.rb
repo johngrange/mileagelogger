7477823ee84be6fa6c616c7d21e92973447b2e15
John Grange <grangemobile@gmail.com>
2012-03-27 12:03:40 -0700

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mileagelogger::Application.config.secret_token = 'a8d7ff20728e4e98f0eb802a2adf13b8102fd8718107af39964dfe5e5423a5b2265c886c3d3e1fd6acd87f1a64c3410bfa8f3513d25383afab818ade9386ee26'
